207c99018cb2265ec3f4a85e5df04e5a01b6ec74
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-01-21 13:21:33 +0530

    :books: Components to be developed

---
+++ App.js
@@ -0,0 +1,17 @@
+/*
+
+  Components will be creating in food ordering app
+
+  Header
+    - Logo
+    - Nav Item
+  Body
+    - Search 
+    - Restaurent Container
+      - Restaurent Cards
+  Footer
+    - Copyright
+    - Links
+    - Contact
+
+*/
