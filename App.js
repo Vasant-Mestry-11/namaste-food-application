077bd72e5ba71e6b7d963dc9de05df009d1bb6cb
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-01-21 13:47:50 +0530

    :construction: Header Component

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /*
 
   Components will be creating in food ordering app
@@ -15,3 +18,43 @@
     - Contact
 
 */
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/* Logo */}
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt="logo"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Body */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
